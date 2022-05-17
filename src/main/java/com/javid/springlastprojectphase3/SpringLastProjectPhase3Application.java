ff28254c33101f764e111197205bf9df530130bf
javid <javid.rafiee@gmail.com>
2022-05-17 13:26:52 +0330

    Initializing project.

---
+++ src/main/java/com/javid/springlastprojectphase3/SpringLastProjectPhase3Application.java
@@ -0,0 +1,13 @@
+package com.javid.springlastprojectphase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLastProjectPhase3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLastProjectPhase3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/javid/springlastprojectphase3/SpringLastProjectPhase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javid.springlastprojectphase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLastProjectPhase3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
