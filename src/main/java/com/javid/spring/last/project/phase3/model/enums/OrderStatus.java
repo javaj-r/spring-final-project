cbb43829afe0c76f97c578cce15cfd320e0e58fb
javid <javid.rafiee@gmail.com>
2022-05-21 12:59:13 +0330

    Adding OrderStatus.

---
+++ src/main/java/com/javid/spring/last/project/phase3/model/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.javid.spring.last.project.phase3.model.enums;
+
+/**
+ * @author javid
+ * Created on 5/1/2022
+ */
+public enum OrderStatus {
+    AWAITING_SUGGESTION, AWAITING_EXPERT_SELECTION, AWAITING_EXPERT_ACHIEVEMENT, STARTED, DONE, PAID
+}
