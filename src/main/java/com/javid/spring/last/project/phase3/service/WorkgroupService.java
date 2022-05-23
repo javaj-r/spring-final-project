dccbfe647dc699a7f60aff33d8dd7c91830ef717
javid <javid.rafiee@gmail.com>
2022-05-23 17:26:36 +0330

    Adding WorkgroupService.

---
+++ src/main/java/com/javid/spring/last/project/phase3/service/WorkgroupService.java
@@ -0,0 +1,14 @@
+package com.javid.spring.last.project.phase3.service;
+
+import com.javid.spring.last.project.phase3.dto.WorkDto;
+
+import java.util.List;
+
+/**
+ * @author javid
+ * Created on 5/21/2022
+ */
+public interface WorkgroupService {
+
+    List<WorkDto> findAll();
+}
