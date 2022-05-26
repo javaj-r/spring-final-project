73d8b146962ec8eb64983c5646d1bcf54c362baf
javid <javid.rafiee@gmail.com>
2022-05-26 23:23:04 +0330

    Adding BadRequestException.

---
+++ src/main/java/com/javid/spring/last/project/phase3/exception/BadRequestException.java
@@ -0,0 +1,27 @@
+package com.javid.spring.last.project.phase3.exception;
+
+/**
+ * @author javid
+ * Created on 5/10/2022
+ */
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException() {
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadRequestException(Throwable cause) {
+        super(cause);
+    }
+
+    public BadRequestException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
