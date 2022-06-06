3cd2691a96ddf5cac3e8e8b548cf49b4ddd12eec
javid <javid.rafiee@gmail.com>
2022-06-06 19:38:03 +0330

    Refactoring BadRequestException. Adding ResponseStatus.

--- src/main/java/com/javid/sfp/exception/BadRequestException.java
+++ src/main/java/com/javid/sfp/exception/BadRequestException.java
@@ -1,9 +1,13 @@
 package com.javid.sfp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author javid
  * Created on 5/10/2022
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
 
     public BadRequestException() {
