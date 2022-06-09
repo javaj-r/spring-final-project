ddd7a4c1f2102a58fab17fb9812c87699f1e63ca
javid <javid.rafiee@gmail.com>
2022-06-09 23:37:40 +0330

    Adding Role class

---
+++ src/main/java/com/javid/sfp/security/Role.java
@@ -0,0 +1,11 @@
+package com.javid.sfp.security;
+
+/**
+ * @author javid
+ * Created on 6/1/2022
+ */
+public class Role {
+    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String EXPERT = "ROLE_EXPERT";
+    public static final String CUSTOMER = "ROLE_CUSTOMER";
+}
