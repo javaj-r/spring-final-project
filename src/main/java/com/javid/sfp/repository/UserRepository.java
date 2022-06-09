61a33a828af33633a289527131c5b19d33dbe951
javid <javid.rafiee@gmail.com>
2022-06-09 23:40:34 +0330

    Updating UserRepository

--- src/main/java/com/javid/sfp/repository/UserRepository.java
+++ src/main/java/com/javid/sfp/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.javid.sfp.repository;
 import com.javid.sfp.model.base.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * @author javid
  * Created on 5/18/2022
@@ -10,4 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
