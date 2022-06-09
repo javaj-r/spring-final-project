9e4eb6bf72c268ca08735e5fcd67f4881ff5bf22
javid <javid.rafiee@gmail.com>
2022-06-09 23:15:56 +0330

    Adding UnauthorizedException

---
+++ src/main/java/com/javid/sfp/exception/UnauthorizedException.java
@@ -0,0 +1,31 @@
+package com.javid.sfp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author javid
+ * Created on 6/5/2022
+ */
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException() {
+    }
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnauthorizedException(Throwable cause) {
+        super(cause);
+    }
+
+    public UnauthorizedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
